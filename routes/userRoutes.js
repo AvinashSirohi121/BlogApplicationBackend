23ed0cb0308aecae4605ff4053321335dc9fef14
AvinashSirohi121 <avinashsirohi86@gmail.com>
2026-06-04 23:05:45 +0530

    Changes in user routes

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -1,9 +1,22 @@
 const express = require("express");
 const router = express.Router();
 
-const {createUser,editUser} = require("../controllers/userController");
-
+const {createUser,editUser,getAllUsers,getUser,deleteUser} = require("../controllers/userController");
+const {getAllPost, createPost} = require("../controllers/postController")
+const {createCategory,getCategory} = require("../controllers/categoryController")
+/* User Routes */
 router.post("/createUser",createUser);
 router.put("/editUser/:id",editUser);
+router.get("/getUser/:id",getUser);
+router.get("/getAllUser",getAllUsers);
+router.delete("/deleteUser/:id",deleteUser);
+
+/* Blog Routes*/
+router.get("/getAllPost",getAllPost);
+router.post("/createPost",createPost);
+
+
+router.post("/createCategory",createCategory);
+router.get("/getCategory",getCategory);
 
 module.exports = router;
