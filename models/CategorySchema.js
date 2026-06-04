6ecbb0ddca44b3c870bcfc11f60c23c116f82593
AvinashSirohi121 <avinashsirohi86@gmail.com>
2026-06-04 23:05:11 +0530

    Changes in category

--- models/CategorySchema.js
+++ models/CategorySchema.js
@@ -3,10 +3,15 @@ const mongoose = require('mongoose');
 const categorySchema = new mongoose.Schema({
     name:{
         type:String,
-        unique:true
+        unique:true,
+        required:true,
+        lowercase:true
     },
-    description:String
+    description:{
+        type:String,
+        required:true
+    }
 
-})
+},{timestaps:true})
 
 module.exports = mongoose.model("Category",categorySchema)
