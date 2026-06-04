7775dd6ab399a1790894faab7ae542c4d89356e1
AvinashSirohi121 <avinashsirohi86@gmail.com>
2026-06-04 23:04:24 +0530

    Added User controllers and routes

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,7 +1,7 @@
 const mongoose = require("mongoose");
 const User = require("../models/UserSchema");
 
-exports.createUser = async(req,res)=>{
+const createUser = async(req,res)=>{
     try{
         const {firstName,lastName,email,mobile,password} = req.body;
         console.log("JSON Body =>",req.body)
@@ -12,16 +12,16 @@ exports.createUser = async(req,res)=>{
                 message:"Kindly provide all details"
             })
         }
-        const existingUserEmail = await User.findOne({email});
-        const existingUserMobile = await User.findOne({mobile});
+        const existingUserEmail = await userExist({email:email});
+        const existingUserMobile = await userExist({mobile:mobile});
 
-        if(existingUserEmail){
+        if(!existingUserEmail){
             return res.status(400).json({
                 success:false,
                 message:"User exist with same email Id"
             })
         }
-        if(existingUserMobile){
+        if(!existingUserMobile){
             return res.status(400).json({
                 success:false,
                 message:"User exist with same mobile number"
@@ -55,7 +55,7 @@ exports.createUser = async(req,res)=>{
     }
 }
 
-exports.editUser = async(req,res)=>{
+const editUser = async(req,res)=>{
     try {
       const id = req.params.id;
       console.log("Id =>",id)
@@ -103,3 +103,98 @@ exports.editUser = async(req,res)=>{
         })
     }
 }
+
+const getUser = async(req,res)=>{
+    try {
+        const {id} = req.params;
+
+        const userDetails = await User.findById({_id:id});
+        if(!userDetails){
+            return res.status(404).json({
+                success:false,
+                message:`Unable to find the User details for UserId : ${id}`
+            })
+        }else{
+            return res.status(200).json({
+                success:true,
+                message:`User details for UserId : ${id}`,
+                data:userDetails
+            })
+        }
+    } catch (error) {
+        console.error("Error in get User details Method =>",error)
+        return res.status(500).json({
+            success:false,
+            message:"Error in Get User Details",
+            error:error.message
+        })
+    }
+}
+
+const getAllUsers = async(req,res)=>{
+    try {
+        const allUserDetails = await User.find({});
+        if(!allUserDetails){
+            return res.status(404).json({
+                success:false,
+                message:"Unable to find the users"
+            })
+        }else{
+            return res.status(200).json({
+                success:false,
+                message:"User details presnet",
+                data:allUserDetails
+            })
+        }
+    } catch (error) {
+        console.error("Error in get All User Details Method =>",error)
+        return res.status(500).json({
+            success:false,
+            message:"Error in Get All User Details Method",
+            error:error.message
+        })
+    }
+}
+
+const deleteUser = async(req,res)=>{
+    try {
+        const {id} = req.params;
+
+        const deleteUser = await User.findByIdAndDelete(id);
+        if(!deleteUser){
+            return res.status(404).json({
+                success:false,
+                message:`Unable to find the User with userId : ${id} to delete`
+            })
+        }else{
+            return res.status(200).json({
+                success:true,
+                message:"User details deleted successfully"
+            })
+        }
+    } catch (error) {
+        console.log("Error in deleting the user details =>",error);
+        return res.status(500).json({
+            success:false,
+            message:"Error in deleting the user details",
+            error:error.message
+        })
+    }
+}
+
+const userExist = async ({ id, email, mobile })=>{
+    if (id) {
+        return await User.findById(id);
+    }
+
+    if (email) {
+        return await User.findOne(email);
+    }
+    if (mobile) {
+        return await User.findOne(mobile);
+    }
+
+    return null;
+}
+
+module.exports ={userExist,deleteUser,getAllUsers,getUser,editUser,createUser}
