252c0a22b9d017dde965de0cd58ac067d7271829
AvinashSirohi121 <avinashsirohi86@gmail.com>
2026-06-06 23:16:10 +0530

    Added the routes for Likes and Comments

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -2,8 +2,11 @@ const express = require("express");
 const router = express.Router();
 
 const {createUser,editUser,getAllUsers,getUser,deleteUser} = require("../controllers/userController");
-const {getAllPost, createPost} = require("../controllers/postController")
-const {createCategory,getCategory} = require("../controllers/categoryController")
+const {getAllPost, createPost,getPost,deletePost,editPost} = require("../controllers/postController");
+const {createCategory,getCategory} = require("../controllers/categoryController");
+const {toggleLikesOnPost} = require("../controllers/likeController");
+const { createComment,editComment,deleteComment,getAllCommentsForPost} = require("../controllers/commentController");
+
 /* User Routes */
 router.post("/createUser",createUser);
 router.put("/editUser/:id",editUser);
@@ -14,9 +17,22 @@ router.delete("/deleteUser/:id",deleteUser);
 /* Blog Routes*/
 router.get("/getAllPost",getAllPost);
 router.post("/createPost",createPost);
+router.get("/getPost/:id",getPost);
+router.put("/editPost/:id",editPost);
+router.delete("/deletePost/:id",deletePost);
 
 
+/*Category Routes*/
 router.post("/createCategory",createCategory);
 router.get("/getCategory",getCategory);
+
+/*Like Routes */
+router.put("/toggleLikesOnPost",toggleLikesOnPost)
+
+/*Comment Routes*/
+router.post("/createComment", createComment)
+router.put("/editComment/:id", editComment)
+router.delete("/deleteComment/:id", deleteComment)
+router.get("/getAllCommentsForPost/:id",getAllCommentsForPost)
 
 module.exports = router;
